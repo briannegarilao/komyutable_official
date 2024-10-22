09b3bcde330e8d1540e8aec1b0d7d5ba2e1860ff
Brianne Garilao <briannegarilao@gmail.com>
2024-10-22 22:26:46 +0800

    Second debug run. Added location tracker

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
 import { Text, View } from "react-native";
+import Map from "../components/Map";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <Map />;
 }
---
+++ components/Map.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { PermissionsAndroid } from "react-native";
+import MapBox, { MapView, Camera, LocationPuck } from "@rnmapbox/maps";
+
+MapBox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || "");
+
+const Map = () => {
+  const requestLocationPermission = async () => {
+    try {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  return (
+    <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
+      <Camera zoomLevel={50} followUserLocation />
+      <LocationPuck
+        puckBearingEnabled
+        puckBearing="heading"
+        pulsing={{ isEnabled: true }}
+      />
+    </MapView>
+  );
+};
+
+export default Map;
